799124c179d271c6a697b7aaa75f8e0f6f82c58c
Jumi Yoon <jums.code@gmail.com>
2023-05-09 14:45:27 -0700

    Add parents and kids seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,49 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+Parent.create([
+    {
+        family_name: "Yoons",
+        phone: 1234567890,
+        service_time: "1st Service"
+    },
+    {
+        family_name: "Leighs",
+        phone: 2345678901,
+        service_time: "1st Service"
+    },
+    {
+        family_name: "Garcias",
+        phone: 3456789012,
+        service_time: "2nd Service"
+    }
+])
+
+Kid.create([
+    {
+        name: "Jumi",
+        parent_id: 1,
+        dietary_restrictions: "None",
+    },
+    {
+        name: "Christy",
+        parent_id: 1,
+        dietary_restrictions: "None",
+    },
+    {
+        name: "James",
+        parent_id: 2,
+        dietary_restrictions: "Gluten-free",
+    },
+    {
+        name: "Jose",
+        parent_id: 3,
+        dietary_restrictions: "Apples",
+    },
+    {
+        name: "Mila",
+        parent_id: 3,
+        dietary_restrictions: "Nuts",
+    }
+])
 
 puts "✅ Done seeding!"
