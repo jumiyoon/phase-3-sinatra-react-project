4582ae1bb4751f7bcd3b22812de9cba58f44e7fe
Jumi Yoon <jums.code@gmail.com>
2023-05-09 14:47:15 -0700

    Add kid routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,54 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+# Kids 
+  
+  get '/kids' do 
+    kids = Kid.all.order(:parent_id)
+    kids.to_json(include: {parent: {only: [:family_name, :phone, :service_time]}})
+
+    
+    # 
+
+  end
+
+  get '/kids/:id' do
+    kid = Kid.find(params[:id])
+    kid.to_json(include: {parent: {only: [:family_name, :phone, :service_time]}})
+  end
+
+  post '/kids' do
+    kid = Kid.create(
+      name: params[:name],
+      parent_id: params[:parent_id],
+      dietary_restrictions: params[:dietary_restrictions]
+      # parent: (
+      #   name: "Default",
+      #   phone: "Default",
+      #   service_time: "Default"
+      # )
+    )
+    kid.to_json
+  end
+
+  patch '/kids/:id' do
+    kid = Kid.find(params[:id])
+    kid.update(
+      dietary_restrictions: params[:dietary_restrictions]
+    )
+    kid.to_json
+  end
+
+  delete '/kids/:id' do
+    kid = Kid.find(params[:id])
+    kid.destroy
+    kid.to_json
+  end
+
+  private
+  def parent_params
+      params.require(:parent).permit(:family_name, :phone, :service_time, kids_attributes: [:name, :dietary_restrictions])
+  end
+
+
 end
