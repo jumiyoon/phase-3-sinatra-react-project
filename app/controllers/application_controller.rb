b53795d44e6d9f7d6891eeae835ddc89b4e6ede7
Jumi Yoon <jums.code@gmail.com>
2023-05-09 14:47:34 -0700

    modify kids route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,12 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
 
 # Kids 
   
   get '/kids' do 
     kids = Kid.all.order(:parent_id)
     kids.to_json(include: {parent: {only: [:family_name, :phone, :service_time]}})
-
-    
-    # 
-
   end
 
   get '/kids/:id' do
@@ -50,10 +42,10 @@ class ApplicationController < Sinatra::Base
     kid.to_json
   end
 
-  private
-  def parent_params
-      params.require(:parent).permit(:family_name, :phone, :service_time, kids_attributes: [:name, :dietary_restrictions])
-  end
+  # private
+  # def parent_params
+  #     params.require(:parent).permit(:family_name, :phone, :service_time, kids_attributes: [:name, :dietary_restrictions])
+  # end
 
 
 end
