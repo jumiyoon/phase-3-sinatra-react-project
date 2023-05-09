9472263db9ae117ee31dded6dad737418413e4ba
Jumi Yoon <jums.code@gmail.com>
2023-05-09 14:46:21 -0700

    migrate kids table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_09_214223) do
+ActiveRecord::Schema.define(version: 2023_05_09_214414) do
+
+  create_table "kids", force: :cascade do |t|
+    t.string "name"
+    t.integer "parent_id"
+    t.string "dietary_restrictions"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "parents", force: :cascade do |t|
     t.string "family_name"
