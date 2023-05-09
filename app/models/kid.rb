eead448d0a7488b7415e6e3582b79299c8a4a085
Jumi Yoon <jums.code@gmail.com>
2023-05-09 14:50:41 -0700

    add functionality to kids.rb

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,5 +1,19 @@
 class Kid < ActiveRecord::Base
- 
+    belongs_to :parent
+
+    accepts_nested_attributes_for :parent
+
+    def parents
+        self.parent.family_name
+    end
+    
+    def service_time
+        self.parent.service_time
+    end
+
+    def parents_phone
+        self.parent.phone
+    end
 
     
 end
